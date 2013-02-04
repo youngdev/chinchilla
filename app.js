578a983075b40b1c98f114aac2f0be79ab9f820f
OpenShift guest <jonathanburger11@gmail.com>
2013-02-04 09:28:52 -0500

    Whitespace improvement

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
 /*
 	Require the basic stuff like the express framework
 */
-var app    	= require('express').createServer(),
-    io	 	= require('socket.io').listen(app),
-    swig   	= require('swig')
-    views  	= require('./routes/views.js'),
+var app     = require('express').createServer(),
+    io      = require('socket.io').listen(app),
+    views   = require('./routes/views.js'),
     scripts = require('./routes/scripts.js'),
     styles  = require('./routes/styles.js'),
     events  = require('./config/events.js'),
@@ -20,16 +19,16 @@ console.log("App started on port", port);
 /* 
 	These are the routes, they control what is sent to the user
 */
-app.get("/", 						    views.mainview);
+app.get("/",                            views.mainview);
 app.get("/artist/:id",                  views.mainview);
 app.get("/charts",                      views.mainview);
 app.get("/album/:id",                   views.mainview);
 app.get("/about",                       views.mainview);
-app.get("/api/script/:scriptname", 		scripts.get);
-app.get("/api/styles/:filename", 		styles.get)
-app.get("/api/artist/:id", 			    views.drawartist);
+app.get("/api/script/:scriptname",      scripts.get);
+app.get("/api/styles/:filename",        styles.get);
+app.get("/api/artist/:id",              views.drawartist);
 app.get("/api/charts",                  views.charts);
-app.get("/api/album/:album", 			views.drawalbum);
+app.get("/api/album/:album",            views.drawalbum);
 app.get("/api/i/:filename",             styles.images.get);
 app.get("/api/error/:code",             views.error);
 app.get("/api/about",                   views.about);
