4446f814ab65558361811f6694c012cd16742263
Jonathan Burger <jonny@vidstatsx.com>
2013-07-03 00:11:34 +0200

    Better search
    Keyboard select, better order for relevant stuff

--- frontend/scripts/search.js
+++ frontend/scripts/search.js
@@ -1,6 +1,7 @@
 search = {
 	calls: [],
 	autocomplete: function(query) {
+		$('#search-results-wrapper').empty();
 		search.calls = [];
 		var types = {
 			artist: {
@@ -9,7 +10,8 @@ search = {
 				sub: 		'primaryGenreName',
 				element: 	$('#results-artists'),
 				link: 		'/artist/$1',
-				id: 		'artistId'
+				id: 		'artistId',
+				image: 		'/api/svg/artist'
 			},
 			album: {
 				iTunesName: 'album',
@@ -17,7 +19,8 @@ search = {
 				sub: 		'artistName',
 				element: 	$('#results-albums'),
 				link: 		'/album/$1',
-				id: 		'collectionId'
+				id: 		'collectionId',
+				image: 		'/api/svg/album'
 			},
 			track: {
 				iTunesName: 'song',
@@ -25,8 +28,33 @@ search = {
 				sub: 		'artistName',
 				element: 	$('#results-tracks'),
 				link: 		'/song/$1',
-				id: 		'trackId'
+				id: 		'trackId',
+				image: 		'/api/svg/playlist'
 			}
+		}
+		var results = {
+			count: 0,
+			objects: []
+		};
+		var allResultsFetched = function(results) {
+			var results = _.sortBy(results.objects, function (result, key) {
+				var one = _s.levenshtein(result.title, query),
+					two = _s.levenshtein(result.sub, query) + 0.5 + (key + 1) % 3; // 0.5 Because title is always more important than sub
+				return Math.min(one, two);
+			});
+			var results = _.map(results, function (result) {
+				var regex 		= new RegExp(query, 'gi');
+				result.title 	= result.title.replace(regex, '<b>' + result.title + '</b>');
+				result.sub 		= result.sub.replace(regex, '<b>' + result.sub + '</b>');
+				return result;
+			})
+			_.each(results, function (result, key) {
+				var html 		= $('<li data-navigate="' + result.link + '"><a><img src="' + result.image + '"><span>' + result.title + '</span><span class="search-result-sub">' + result.sub + '</span></a></li>');
+				if (key == 0) {
+					var html 	= $(html).addClass('search-selected')
+				}
+				$('#search-results-wrapper').append(html);
+			});
 		}
 		$.each(types, function(type, info) 
 			{
@@ -40,14 +68,19 @@ search = {
 						},
 						dataType: 'jsonp',
 						success: function(json) {
-							$(info.element).empty();
-							$.each(json.results, function(key, result) {
-								var div = $('<div>', { class: 'search-result' });
-										  $('<div>', { class: 'search-result-title' }).text(result[info.title]).appendTo(div);
-										  $('<div>', { class: 'search-result-sub' }).text(result[info.sub]).appendTo(div);
-								var a   = $('<a>'  , { 'data-navigate' : info.link.replace('$1', result[info.id])}).html(div)
-								a.appendTo(info.element)		  
-							})
+							_.each(json.results, function(result) {
+								results.objects.push({
+									title: 		result[info.title],
+									sub: 		result[info.sub],
+									type: 		type,
+									link: 		info.link.replace('$1', result[info.id]),
+									image: 		info.image
+								});
+							});
+							results.count++;
+							if (results.count == 3) {
+								allResultsFetched(results)
+							}
 						}
 					}
 				)
