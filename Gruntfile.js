41537e4d2a658661f4d690c4dc723c845eb85688
Jonathan Burger <jonny@vidstatsx.com>
2013-07-04 16:54:46 +0200

    Workflow improvement: Grunt
    Includes grunt.js, which reloads the server, concantinates, minifies
    etc. all necessary files

---
+++ Gruntfile.js
@@ -0,0 +1,78 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    watch: {
+        src: {
+            files: ['frontend/scripts/*', '!frontend/scripts/app.min.js'],
+            tasks: ['concat']
+        },
+        backend: {
+            files: ['routes/*', 'sites/*', 'db/*', 'config/*', 'auth/*'],
+            tasks: ['forever:restart']
+        },
+        css: {
+            files: ['frontend/styles/*.less'],
+            tasks: ['less']
+        },
+        options: {
+            livereload: true
+        }
+    },
+    concat: {
+      options: {
+        separator: ';'
+      },
+      dist: {
+        src: [
+        'frontend/scripts/underscore.js',
+        'frontend/scripts/helpers.js',
+        'frontend/scripts/underscore.js',
+        'frontend/scripts/helpers.js',
+        'frontend/scripts/fetchFeeds.js',
+        'frontend/scripts/navigation.js',
+        'frontend/scripts/socket.io.js',
+        'frontend/scripts/notifications.js',
+        'frontend/scripts/sockets.js',
+        'frontend/scripts/templates.js',
+        'frontend/scripts/player.js',
+        'frontend/scripts/search.js',
+        'frontend/scripts/add-tracks.js',
+        'frontend/scripts/import.js',
+        'frontend/scripts/errors.js',
+        'frontend/scripts/recognition.js',
+        'frontend/scripts/swfobject.js',
+        'frontend/scripts/libdom.js',
+        'frontend/scripts/library.js',
+        'frontend/scripts/UI.js',
+        'frontend/scripts/pubsub.js',
+        'frontend/scripts/local.js'
+        ],
+        dest: 'frontend/scripts/app.min.js'
+      }
+    },
+    forever: {
+        options: {
+            index: 'app.js',
+            logDir: 'node_modules/trash'
+        }
+    },
+    less: {
+      production: {
+        options: {
+          yuicompress: true
+        },
+        files: {
+            "frontend/css/main.css": "frontend/styles/main.less"
+        }
+      }
+    }
+  });
+
+  // Load the plugin that provides the "uglify" task.
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-less');
+  grunt.loadNpmTasks('grunt-forever');
+  grunt.registerTask('default', ['concat', 'less']);
+};
