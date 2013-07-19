505a81368daec8585baf9631d754cece5eef07e3
Jonathan Burger <jonny@vidstatsx.com>
2013-07-19 21:34:13 +0200

    Ignore auth

--- auth/auth.js
+++
@@ -1,3 +0,0 @@
-this.auth = "jonny:zinedinezidane11@127.0.0.1:27017/chinchilla"
-//this.auth = "jonny:zinedinezidane11@ds047307.mongolab.com:47307/chinchilla?auto_reconnect";
-//this.auth = "jonnyburger:zinedinezidane11@mongo.onmodulus.net:27017/abiwu3hY"
