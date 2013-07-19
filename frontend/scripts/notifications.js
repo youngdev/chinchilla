49de7ebdb0d2848d935c316d8347252f874c54fb
Jonathan Burger <jonny@vidstatsx.com>
2013-07-19 20:27:32 +0200

    Better recognition/ video selection

--- frontend/scripts/notifications.js
+++ frontend/scripts/notifications.js
@@ -4,5 +4,10 @@ notifications = {
 			Set the given HTML as content
 		*/
 		$('#statusbar').html(html + ' <span class="close-notification">Dismiss</span>').css('display', 'inline');
-	}
+		clearTimeout(notifications.timeout);
+		notifications.timeout = setTimeout(function() {
+			$('#statusbar').fadeOut()
+		}, 5000);
+	},
+	timeout: setTimeout(function() {}, 999999999)
 } 
