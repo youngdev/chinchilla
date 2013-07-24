a6955bb9b672e103d79fd0b7e05ab621094c5451
Jonathan Burger <jonny@vidstatsx.com>
2013-07-24 21:26:35 +0200

    Little change to see if git hook works

--- config/webhook.js
+++ config/webhook.js
@@ -1,4 +1,7 @@
 exports.push = function (request, response) {
+	/*
+		This auto-reloads the server. Let's see if it works...
+	*/
 	console.log(request.params.payload);
 	var exec = require('child_process').exec;
 	var puts = function(error, stdout, stderr) { console.log(stdout); }
